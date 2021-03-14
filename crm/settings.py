ee8ca591367064a5c593e805e338204b03a94eac
rikicop <ruperto1@protonmail.com>
2021-03-13 20:40:35 -0400

    db queries

--- crm/settings.py
+++ crm/settings.py
@@ -108,3 +108,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, 'static')
+
+]
+
+SESSION_COOKIE_SECURE = True
+

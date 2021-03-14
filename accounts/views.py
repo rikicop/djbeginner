e68447ce25e12d1ef2c6726b08930e47aceef87c
rikicop <ruperto1@protonmail.com>
2021-03-14 19:33:39 -0400

    crud

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 def home(request):
     orders = Order.objects.all()
@@ -23,5 +24,49 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html',{'products':products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    #query customer childs
+    orders= customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+    return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    
+    form = OrderForm()
+    if request.method == 'POST':
+        #print('Printing Post:', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        #print('Printing Post:', request.POST)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
+
